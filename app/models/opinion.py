7184638d9b4c325714e86bc71047fdbb8d5b232f
malwinakochman <s223011@student.uek.krakow.pl>
2022-06-10 02:06:44 +0200

    2022-06-10 MK Homework 3

--- app/models/opinion.py
+++ app/models/opinion.py
@@ -1,3 +1,15 @@
 class Opinion():
-    def __init__():
-        pass
+    def __init__(self, product_id, author, recommendation, stars, content, useful, useless, published, purchased, pros, cons):
+        self.product_id = product_id
+        self.author = author
+        self.recommendation = recommendation
+        self.stars = stars
+        self.content = content
+        self.useful = useful
+        self.useless = useless
+        self.published = published
+        self.purchased = purchased
+        self.pros = pros
+        self.cons = cons
+        
+    
--- app/models/product.py
+++ app/models/product.py
@@ -1,3 +1,8 @@
+from numpy import product
+
+
 class Product():
-    def __init__():
-        pass
+    def __init__(self, product_id):
+        self.product_id = product_id
+      
+        
